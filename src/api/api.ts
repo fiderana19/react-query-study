b5dacc5bd8871b0589737f7e68ee3d2def6cea6c
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2024-11-30 11:03:57 +0300

    feat: Added the dev tools

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,18 @@
-const fetchPlayers = async () => {
+export const fetchPlayers = async () => {
     const response = await fetch(`http://localhost:3000/players?_sort=-id`);
     
     const playerData = await response.json();
     return playerData;
 };
     
-const fetchTags = async () => {
+export const fetchTags = async () => {
     const response = await fetch(`http://localhost:3000/tags?_sort=-id`);
     
     const tagsData = await response.json();
     return tagsData;
 };
 
-const addPlayer = async (player: any) => {
+export const addPlayer = async (player: any) => {
     const response = await fetch(`http://localhost:3000/players`, {
         method: "POST",
         headers: {
