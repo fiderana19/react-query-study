c3673bab93f03d8d0f5b1528a40abc3ed489cdc6
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2024-12-01 19:56:05 +0300

    feat: Tested the useMutation

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -1,35 +1,79 @@
-import { useQuery } from "@tanstack/react-query"
-import { fetchPlayers, fetchTags } from "../api/api"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { addPlayer, fetchPlayers, fetchTags } from "../api/api"
+import { useState } from "react"
 
 function Player() {
+  const [credentials,setCredentials] = useState<any>({ title: '', tags: [] })
 
   const { data: players, isLoading, isError, error } = useQuery({
     queryKey: ["players"],
     queryFn: fetchPlayers,
-    staleTime: Infinity,
+    staleTime: Infinity
   })
 
   const { data: tags } = useQuery({
     queryKey: ["tags"],
     queryFn: fetchTags,
-    staleTime: Infinity,
   })
+
+  const { 
+    mutate,
+    isError: isAddError, 
+    isPending, 
+    error: addPlayerError,
+    reset
+  }  = useMutation({
+    mutationFn: addPlayer,
+    onMutate() {
+      return {id: 1}
+    },
+  })
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(credentials);
+
+    mutate(credentials);
+    setCredentials({title: '', tags: []})
+    e.target.reset();
+  }
+
+  const onChange = (e: any) => {
+    const { name, value } = e.target;
+    console.log(e.target.value)
+    setCredentials((prev: any) => ({...prev, [name]: value}));
+  }
+
+  const onSelectChange = (e: any) => {
+    setCredentials((prev: any) => ({...prev, tags: [e.target.name]}))
+  }
 
   return (
       <div className="mx-auto">
-        <div className="rounded border p-2 bg-gray-100">
+        <form onSubmit={handleSubmit} className="rounded border p-2 bg-gray-100">
             <div className="text-xl font-bold my-1" >New player</div>
-            <input type="text" className="bg-transparent border rounded px-4 py-1 mb-4" placeholder="Type the player name..." />
+            <input 
+              value={credentials.title}
+              onChange={onChange}
+              name="title"
+              type="text" 
+              className="bg-transparent border rounded px-4 py-1 mb-4" 
+              placeholder="Type the player name..." />
             <div className="flex gap-1 my-1">
               {tags?.map((tag: any) => {
                 return <div key={tag} className="rounded-full border border-gray-100 bg-gray-300 px-2 py-0.5 text-xs text-gray-900 flex gap-1 items-center">
-                            <input type="checkbox" name={tag} id={tag} />
+                            <input onChange={onSelectChange} type="checkbox" name={tag} id={tag} />
                             <label htmlFor={tag}>#{tag}</label>
                         </div>
               })}
             </div>
-            <button className="rounded border border-green-400 bg-green-600 hover:bg-green-700 transition-colors text-white px-4 py-1.5 w-1/3 mt-4">Add</button>
-        </div>
+            <button 
+              className="rounded border border-green-400 bg-green-600 hover:bg-green-700 transition-colors text-white px-4 py-1.5 w-1/3 mt-4"
+              type="submit"
+            >
+              Add
+            </button>
+        </form>
         {isLoading && <div className="px-4 py-2 border border-gray-100 rounded bg-gray-200 text-gray-400 my-1">Loading...</div>}
         {isError && <div className="px-4 py-2 border border-red-100 rounded bg-red-200 text-red-400 my-1">{error?.message}</div>}
         <div className="text-3xl font-bold my-4" >PLAYERS</div>
@@ -38,7 +82,7 @@ function Player() {
             <div className="font-bold">{player.title}</div>
             <div className="flex gap-1">
               {player.tags.map((tag: any) => {
-                return <div className="rounded-full border border-gray-100 bg-gray-300 px-2 py-0.5 text-xs text-gray-900">
+                return <div key={tag} className="rounded-full border border-gray-100 bg-gray-300 px-2 py-0.5 text-xs text-gray-900">
                     #{tag}
                   </div>
               })}
