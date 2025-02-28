529505042cb1ae362ee3741a74ba655505823c9a
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-02-28 11:32:09 +0300

    feat: Added a hook form package

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Form from "./components/Form"
 import Player from "./components/Players"
 
 function App() {
   return (
     <div className="flex flex-col justify-center h-screen text-center">
+      <Form />
       <Player />
     </div>
   )
---
+++ src/components/Form.tsx
@@ -0,0 +1,13 @@
+import '../../node_modules/react-toastify/dist/ReactToastify.css';
+import '../../node_modules/react-toastify/dist/ReactToastify.min.css';
+
+
+function Form() {
+  return (
+      <div className='p-2 border border-gray-500 rounded'>
+        form
+      </div>
+  )
+}
+
+export default Form;
